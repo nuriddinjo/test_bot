50126a9ba4f3b987c1c3b0ec0bbbc3f2c3680ff5
nuriddinjo <etobandano1@gmail.com>
2024-03-04 10:36:54 +0500

    2 ta handler yozdik

--- bot.py
+++ bot.py
@@ -1,3 +1,22 @@
 # Aiogram
 # pyTelegramBotApi
 
+from telebot import TeleBot
+from telebot.types import Message
+
+bot = TeleBot('7162023744:AAGMSH_CgbbMuRN_DDvJHElgerOYTD8wGeQ')
+
+@bot.message_handler(commands=['start', 'help'])
+def start(message: Message):
+    chat_id = message.chat.id
+    full_name = message.from_user.full_name
+    bot.send_message(chat_id, f"Assalomu alaykum {full_name} ")
+
+
+@bot.message_handler(commands=['text','photo','video', 'animation','stiker'])
+def reaction_to_message(message: Message):
+    chat_id = message.chat_id
+    bot.copy_message(5754538843, chat_id, message)
+
+
+bot.polling()
