9724d7a818ffdd23843d1f55e31cc44ebac2bdc0
nuriddinjo <etobandano1@gmail.com>
2024-03-04 21:01:43 +0500

    homework bot yaratildi

--- homework_bot.py
+++ homework_bot.py
@@ -1,10 +1,7 @@
-# Aiogram
-# pyTelegramBotApi
-
 from telebot import TeleBot
 from telebot.types import Message
 
-bot = TeleBot('7162023744:AAGMSH_CgbbMuRN_DDvJHElgerOYTD8wGeQ')
+bot = TeleBot('7041601837:AAEobyHVJLZIqPYJC6S0SY4sUnR_jipOGp0')
 
 @bot.message_handler(commands=['start', 'help'])
 def start(message: Message):
@@ -12,22 +9,9 @@ def start(message: Message):
     full_name = message.from_user.full_name
     bot.send_message(chat_id, f"Assalomu alaykum {full_name} ")
 
-@bot.message_handler(commands=['stop'])
-def stop(message: Message):
-    chat_id = message.chat.id
-    full_name = message.from_user.full_name
-    bot.send_message(chat_id, f"Bot toxtatildi {full_name}")
-
-
 @bot.message_handler(content_types=['text', 'photo', 'video', 'animation', 'sticker'])
 def reaction_to_message(message: Message):
     chat_id = message.chat.id
     bot.copy_message(chat_id, chat_id, message.message_id)
-    # bot.send_message(chat_id, message.text)
-
-
-
 
 bot.polling()
-
-
